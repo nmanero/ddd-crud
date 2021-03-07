73ef25de6c02939f06b07633ef65e38020f22f43
Nicolas Manero <nicolas.manero@4tic.com>
2021-03-07 15:42:17 +0100

    feat: persisting the dto after validating the value object

--- src/modules/commands/post.command.ts
+++ src/modules/commands/post.command.ts
@@ -1,10 +1,13 @@
 import { ValueObject } from '../valueobjects/valueobject.interface';
 import { CustomObjectsFactory } from '../valueobjects/custom/customobjects.factory';
+import { postService } from '../services/services.crud'
 
 const postCommand = async (request: Request, reply: Response) => {
     const valueObjectsFactory = new CustomObjectsFactory(request.params.entity, request.query);
     const entity : ValueObject = valueObjectsFactory.instantiate();
     entity.validate();
+    postService(entity.toDTO());
+
     return {
         success: true,
         entity: entity.whoAmI()
