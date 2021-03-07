60980d4601fc0dea5d53bce17fb8d43301f6bb04
Nicolas Manero <nicolas.manero@4tic.com>
2021-03-07 15:30:21 +0100

    feat: Use of query params in order to make easier the testing of the resource

--- src/modules/commands/post.command.ts
+++ src/modules/commands/post.command.ts
@@ -2,7 +2,7 @@ import { ValueObject } from '../valueobjects/valueobject.interface';
 import { CustomObjectsFactory } from '../valueobjects/custom/customobjects.factory';
 
 const postCommand = async (request: Request, reply: Response) => {
-    const valueObjectsFactory = new CustomObjectsFactory(request.params.entity, request.params);
+    const valueObjectsFactory = new CustomObjectsFactory(request.params.entity, request.query);
     const entity : ValueObject = valueObjectsFactory.instantiate();
     entity.validate();
     return { hello: 'world POST ' + entity.whoAmI() }
