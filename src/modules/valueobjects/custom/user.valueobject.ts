12fff560f57534de0af6deb68f3d30cc0c40a9b2
Nicolas Manero <nicolas.manero@4tic.com>
2021-03-07 15:19:17 +0100

    feat: Added properties command, name and surname for User and added also implementation of whoAmI method

--- src/modules/valueobjects/custom/user.valueobject.ts
+++ src/modules/valueobjects/custom/user.valueobject.ts
@@ -2,9 +2,13 @@ import { ValueObject } from "../valueobject.interface";
 import { Domain } from "../../domains/domain.interface";
 
 export class User implements ValueObject {
-    name: String;
-    constructor(name: String) {
-        this.name = name;
+    private command = 'user';
+    private name: String;
+    private surname: String;
+
+    constructor(body: any) {
+        this.name = body.name;
+        this.surname = body.surname;
     }
 
     validate(): void {
@@ -14,5 +18,9 @@ export class User implements ValueObject {
     toDTO(): Domain {
         console.log('Convert to DTO');
         return null;
+    }
+
+    whoAmI(): String {
+        return this.command;
     }
 }
