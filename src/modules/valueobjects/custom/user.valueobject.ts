48a5bb9914aee5a4ecf112bcc7737f04d885aa9c
Nicolas Manero <nicolas.manero@4tic.com>
2021-03-07 13:12:38 +0100

    feat: Added custom User as valueObject

---
+++ src/modules/valueobjects/custom/user.valueobject.ts
@@ -0,0 +1,18 @@
+import { ValueObject } from "../valueobject.interface";
+import { Domain } from "../../domains/domain.interface";
+
+class User implements ValueObject {
+    name: String;
+    constructor(name: String) {
+        this.name = name;
+    }
+
+    validate(): void {
+        console.log('Valid User');
+    }
+
+    toDTO(): Domain {
+        console.log('Convert to DTO');
+        return null;
+    }
+}
