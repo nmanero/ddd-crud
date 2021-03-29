1f08167b97b25f98ef46c304302c1b66c2df8938
Sergio Gragera <sergiogragera@gmail.com>
2021-03-30 00:04:48 +0200

    feat: create singleton local data instance with collections to differentiate entities

--- src/modules/infraestructure/repositories/custom/local.array.repository.ts
+++ src/modules/infraestructure/repositories/custom/local.array.repository.ts
@@ -3,15 +3,42 @@ import {Domain} from "../../../domains/domain";
 import {Hashmap} from "../../../DTO/hashmap";
 
 export class LocalArrayRepository implements Repository {
-    private items: Hashmap[] = [{ hello: 'world' }];
+    private entityName: string;
+
+    constructor(entityName: string) {
+        this.entityName = entityName;
+    }
 
     async getAll(): Promise<Domain[]> {
         //TODO: map with mapper from Hashmap to Domain
-        return this.items.map(i => i as Domain);
+        return LocalArrayData.getInstance().getCollection(this.entityName).map(i => i as Domain);
     }
 
     async add(item: Domain): Promise<Domain> {
-        this.items.push(item.toDTO());
+        LocalArrayData.getInstance().getCollection(this.entityName).push(item.toDTO());
         return item;
+    }
+}
+
+class LocalArrayData {
+    private static instance: LocalArrayData;
+    private items: { [key: string]: Hashmap[] } = {};
+
+    private constructor() {
+    }
+
+    public static getInstance(): LocalArrayData {
+        if (!LocalArrayData.instance) {
+            LocalArrayData.instance = new LocalArrayData();
+        }
+
+        return LocalArrayData.instance;
+    }
+
+    getCollection(name: string): Hashmap[] {
+        if (!this.items[name]) {
+            this.items[name] = []
+        }
+        return this.items[name];
     }
 }
