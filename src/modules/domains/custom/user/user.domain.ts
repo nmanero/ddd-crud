7276697bb780e5c13c6fd91c6a960f7070977d12
Sergio Gragera <sergiogragera@gmail.com>
2021-04-03 16:11:04 +0200

    feat: change string type for user.id to UUID value object asserted

--- src/modules/domains/custom/user/user.domain.ts
+++ src/modules/domains/custom/user/user.domain.ts
@@ -3,14 +3,15 @@ import {Name} from "../../../DTO/value-object/name";
 import {Surname} from "../../../DTO/value-object/surname";
 import {Hashmap} from "../../../DTO/hashmap";
 import { v4 as uuid } from 'uuid';
+import {UUID} from "../../../DTO/value-object/uuid";
 
 export class User implements Domain {
-    private readonly _id: string;
+    private readonly _id: UUID;
     private readonly _name: Name;
     private readonly _surname: Surname;
 
-    public constructor(name: Name, surname: Surname, id?: string) {
-        this._id = id || uuid();
+    public constructor(name: Name, surname: Surname, id?: UUID) {
+        this._id = id || new UUID(uuid());
         this._name = name;
         this._surname = surname;
     }
@@ -33,6 +34,6 @@ export class User implements Domain {
     }
 
     public whoAmI(): String {
-        return this._id;
+        return this._id.value;
     }
 }
